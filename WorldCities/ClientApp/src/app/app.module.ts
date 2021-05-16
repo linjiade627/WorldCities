eb0fa29ed89a59bf030d6edb5bb536f05a5ac6fe
林加德 <15861349798@163.com>
2021-05-16 23:20:39 +0800

    20210516

--- WorldCities/ClientApp/src/app/app.module.ts
+++ WorldCities/ClientApp/src/app/app.module.ts
@@ -7,20 +7,23 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { CitiesComponent } from './cities/cities.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'cities', component: CitiesComponent }
     ])
   ],
   providers: [],
